814e2c30135eff084dc28f96ff72b8814c28188c
Nik Ze <zebolovnik@yandex.ru>
2023-02-03 01:06:08 +1000

    Add autotests

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,16 @@
 package ru.netology
-
+/*
+ВОПРОС ПРЕПОДАВАТЕЛЮ:
+    При просмотре отчета покрытия тестов в браузере конечные ссылки с функциями не кликабельны - т.е. код с подсветкой
+    не могу увидеть. Соответственно не вижу какие ветки закрыты тестами, какие - не до конца, а какие вовсе не затронуты.
+    Помогите, пожалуйста.
+ */
 fun main() {
-    commissions(100_000, TYPE_MASTERCARD, 0)
-    commissions(76000)
-    commissions(500_000, TYPE_MAESTRO)
+    println("Сумма комиссии составит: ${commissions(100_000, TYPE_MASTERCARD, 0)} руб.")
+    println("Сумма комиссии составит: ${commissions(76000)} руб.")
+    println("Сумма комиссии составит: ${commissions(500_000, TYPE_MAESTRO)} руб.")
+    println("Сумма комиссии составит: ${commissions(100_000, TYPE_MAESTRO, -1000_000)} руб.")
+    println("Сумма комиссии составит: ${commissions(500_000, "Сбер", 0)} руб.")
 }
 
 const val LIMIT_CARD = 75_000 // лимит в календарный мес, после которого начисл комиссия
@@ -11,20 +18,29 @@ const val PERCENT = 0.6 // процентКомиссии 0,6%
 const val TYPE_MASTERCARD = "MasterCard"
 const val TYPE_MAESTRO = "Maestro"
 const val TYPE_VK = "VK Pay"
+const val ERROR_CARD = -1
+const val ERROR_LIMIT = -2
 
 fun commissions(
     transferAmount: Int, // сумма совершаемого перевода
     typeCard: String = TYPE_VK, // тип карты/счёта
     amountPerMonth: Int = 0 // сумма всех переводов в этом месяце
-) {
+): Int {
     val sum = amountPerMonth + transferAmount
     val calcCommission = transferAmount * (commissionByCardType(typeCard) / 100) // расчет суммы комиссии
-    val resultCommission = if (sum > LIMIT_CARD && typeCard != TYPE_VK) calcCommission + 20 else 0.0
-    return println("Сумма комиссии составит: ${resultCommission.toInt()} руб.")
+    return when (typeCard) {
+        TYPE_MASTERCARD, TYPE_MAESTRO, TYPE_VK -> when {
+            sum > LIMIT_CARD && typeCard != TYPE_VK -> (calcCommission + 20).toInt()
+            sum >= LIMIT_CARD && typeCard == TYPE_VK -> 0
+            sum == LIMIT_CARD -> 0
+            else -> ERROR_LIMIT
+        }
+        else -> ERROR_CARD
+    }
 }
 
 fun commissionByCardType(typeCard: String) = when (typeCard) {
     TYPE_VK -> 0.0
     TYPE_MASTERCARD, TYPE_MAESTRO -> PERCENT
-    else -> 0.0
+    else -> ERROR_CARD.toDouble()
 }
---
+++ src/test/kotlin/ru/netology/MainKtTest.kt
@@ -0,0 +1,108 @@
+package ru.netology
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MainKtTest {
+
+    @Test
+    fun commissionsMasterCard() {
+        val type = TYPE_MASTERCARD
+        val transferAmount = 100_000
+        val amountPerMonth = 0
+
+        val result = commissions(
+            typeCard = type,
+            transferAmount = transferAmount,
+            amountPerMonth = amountPerMonth
+        )
+
+        assertEquals(620, result)
+    }
+    @Test
+    fun notCommissionsMasterCard() {
+        val type = TYPE_MASTERCARD
+        val transferAmount = 75_000
+        val amountPerMonth = 0
+
+        val result = commissions(
+            typeCard = type,
+            transferAmount = transferAmount,
+            amountPerMonth = amountPerMonth
+        )
+
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun commissionsMaestro() {
+        val type = TYPE_MAESTRO
+        val transferAmount = 500_000
+        val amountPerMonth = 0
+
+        val result = commissions(
+            typeCard = type,
+            transferAmount = transferAmount,
+            amountPerMonth = amountPerMonth
+        )
+
+        assertEquals(3020, result)
+    }
+    @Test
+    fun notCommissionsMaestro() {
+        val type = TYPE_MAESTRO
+        val transferAmount = 75_000
+        val amountPerMonth = 0
+
+        val result = commissions(
+            typeCard = type,
+            transferAmount = transferAmount,
+            amountPerMonth = amountPerMonth
+        )
+
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun commissionsVk() {
+        val type = TYPE_VK
+        val transferAmount = 100_000
+        val amountPerMonth = 1000_000
+
+        val result = commissions(
+            typeCard = type,
+            transferAmount = transferAmount,
+            amountPerMonth = amountPerMonth
+        )
+
+        assertEquals(0, result)
+    }
+    @Test
+    fun testWrongLimit() {
+        val transferAmount = 0
+        val amountPerMonth = -1
+
+        val result = commissions(
+            transferAmount = transferAmount,
+            amountPerMonth = amountPerMonth
+        )
+
+        assertEquals(ERROR_LIMIT, result)
+    }
+
+    @Test
+    fun testWrongCard() {
+        val type = "MIR"
+        val transferAmount = 75_000
+        val amountPerMonth = 0
+
+        val result = commissions(
+            typeCard = type,
+            transferAmount = transferAmount,
+            amountPerMonth = amountPerMonth
+        )
+
+        assertEquals(ERROR_CARD, result)
+    }
+}
